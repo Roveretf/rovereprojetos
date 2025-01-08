81ecf8b1b8e2af988a41086c198616a4f6561e82
Roveretf <roveretf@saude.sc.gov.br>
2025-01-08 17:47:26 -0300

    Delete pages directory

--- pages/sideBar/scriptSidebar.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  fetch("../pages/sideBar/sidebar.html")
-    .then(response => response.text())
-    .then(data => {
-      document.getElementById("sidebar-container").innerHTML = data;
-    })
-    .catch(error => console.error("Error loading sidebar:", error));
-});
